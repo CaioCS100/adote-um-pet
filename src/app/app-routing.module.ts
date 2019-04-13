560833eea189f7f956eefb2ae65d1728605f3d55
Caio Cesar Silva <caio.cesar.s100@gmail.com>
2019-04-13 10:47:19 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'tela-inicial', loadChildren: './tela-inicial/tela-inicial.module#TelaInicialPageModule' },
 ];
 
 @NgModule({
